ef2d2433a7cf8491db40a629b7a5db21bcf51a68
NAVEENK30 <ucgnavin02@gmail.com>
2024-12-12 12:25:17 -0500

    hi

---
+++ style.js
@@ -0,0 +1,102 @@
+var a=document.querySelector(".img1")
+var b=document.querySelector(".overlay")
+var c=document.querySelector(".img2")
+var b=document.querySelector(".overlay")
+var e=document.querySelector(".img3")
+var b=document.querySelector(".overlay")
+var f=document.querySelector(".img4")
+var b=document.querySelector(".overlay")
+var g=document.querySelector(".img5")
+var b=document.querySelector(".overlay")
+var h=document.querySelector(".img6")
+var b=document.querySelector(".overlay")
+var i=document.querySelector(".img7")
+var b=document.querySelector(".overlay")
+var j=document.querySelector(".img8")
+var b=document.querySelector(".overlay")
+var k=document.querySelector(".img9")
+var b=document.querySelector(".overlay")
+
+function one(){
+    a.style.display="block"
+    b.style.display="block"
+}
+function two(){
+    c.style.display="block"
+    b.style.display="block"
+}
+
+function three(){
+    e.style.display="block"
+    b.style.display="block"
+}
+
+function four(){
+    f.style.display="block"
+    b.style.display="block"
+}
+
+function five(){
+    g.style.display="block"
+    b.style.display="block"
+}
+
+function six(){
+    h.style.display="block"
+    b.style.display="block"
+}
+
+function seven(){
+    i.style.display="block"
+    b.style.display="block"
+}
+
+function eight(){
+    j.style.display="block"
+    b.style.display="block"
+}
+
+function nine(){
+    k.style.display="block"
+    b.style.display="block"
+}
+
+
+
+
+function cancel(){
+     a.style.display="none"
+     b.style.display="none"
+}
+ function cancel1(){
+      c.style.display="none"
+      b.style.display="none"
+ }
+function cancel2(){
+     e.style.display="none"
+     b.style.display="none"
+}
+ function cancel3(){
+      f.style.display="none"
+     b.style.display="none"
+}
+function cancel4(){
+     g.style.display="none"
+    b.style.display="none"
+}
+function cancel5(){
+     h.style.display="none"
+    b.style.display="none"
+}
+function cancel6(){
+     i.style.display="none"
+    b.style.display="none"
+}
+function cancel7(){
+     j.style.display="none"
+    b.style.display="none"
+}
+function cancel8(){
+     k.style.display="none"
+     b.style.display="none"
+ }
